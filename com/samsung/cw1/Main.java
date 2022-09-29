ab877726a4ce5b9682320b4676c7d6e68ba5f8a4
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-09-29 19:49:22 +0300

    add

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -1,13 +1,23 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         byte aByte = -128;
         short aShort = 30000;
         int aInt = 2000000000;
+        long aLong = 20000000000L;
         System.out.println(aByte);
         System.out.println(aShort);
         System.out.println(aInt);
+        System.out.println(aLong);
+        double  aDouble = 8.3434343443;
+        System.out.println(aDouble);
+        Scanner in = new Scanner(System.in);
+        aDouble = in.nextDouble();
+        System.out.println(aDouble);
+
 
     }
 }
