38fe45fcdf93392a05a13a09844b815d989b243b
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-09-29 19:36:57 +0300

    add

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -2,6 +2,12 @@ package com.samsung.cw1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell ");
+        byte aByte = -128;
+        short aShort = 30000;
+        int aInt = 2000000000;
+        System.out.println(aByte);
+        System.out.println(aShort);
+        System.out.println(aInt);
+
     }
 }
