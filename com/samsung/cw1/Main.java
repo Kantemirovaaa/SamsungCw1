84da19a5d3489b2e22034f8030a069ac393cce4f
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-04 22:00:32 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -1,12 +1,28 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean aBool = true;
         //>, <, ==, !=, >=, <=
-        boolean aBoolean = true;
-        System.out.println(!aBoolean);
+        //boolean aBoolean = true;
+        /*System.out.println(!aBoolean);
         aBoolean = 5 > 6;
-        System.out.println(aBoolean);
+        System.out.println(aBoolean);*/
+        /*Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a > b ? a : b);
+        //aBool = 0 < n && n < 10;*/
+        //System.out.println(aBool);
+        // ||- исключающее ИЛИ
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        String res = ( ( y > 0) && ( x * x + y * y > 4) && (x < 2) && (y < x)) ? "YES" : "NO";
+        System.out.println(res);
+        Math.pow(2.0, 5);
 
     }
 }
