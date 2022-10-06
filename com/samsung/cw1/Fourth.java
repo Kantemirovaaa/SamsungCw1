d97808cf5506b4da76cfb32de67b304849979e8e
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-06 17:53:34 +0300

    END

--- com/samsung/cw1/Fourth.java
+++ com/samsung/cw1/Fourth.java
@@ -1,5 +1,12 @@
 package com.samsung.cw1;
 
-public class Fourth {
+import java.util.Scanner;
 
+public class Fourth {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x;
+        x = sc.nextInt();
+        System.out.println((x >= 100 && x <= 999 && (x % 5 == 0)) ? "true" : "false" );
+    }
 }
