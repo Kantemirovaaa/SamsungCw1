ae3bc0dd6df56ae11b131489071679fb231c9185
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-06 19:44:24 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -5,13 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int nextInt = scanner.nextInt();
+       /* int nextInt = scanner.nextInt();
         if (nextInt > 5){
             System.out.println("nextInt > 5");
             System.out.println("!");
         } else {
             System.out.println("nextInt < 5");
             System.out.println("!");
+        }*/
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a);
+            }else {
+                System.out.println(c);
+            }
+        } else {
+            if (b > c) {
+                System.out.println(b);
+            }
+            else {
+                System.out.println();
+            }
         }
     }
 }
