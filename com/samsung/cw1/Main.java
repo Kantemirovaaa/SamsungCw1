5fe7d42522e12dc8dd89298f0b89bdd9d7355981
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-06 19:53:59 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -22,7 +22,37 @@ public class Main {
 
         else if (b > c) System.out.println(b);
            else System.out.println();
+
 */
+        int n = scanner.nextInt();
+        switch(n){
+            case 1:
+                System.out.println("Январь");
+            case 2:
+                System.out.println("Февраль");
+            case 3:
+                System.out.println("Март");
+            case 4:
+                System.out.println("Апрель");
+            case 5:
+                System.out.println("Май");
+            case 6:
+                System.out.println("Июнь");
+            case 7:
+                System.out.println("Июль");
+            case 8:
+                System.out.println("Август");
+            case 9:
+                System.out.println("Сентябрь");
+            case 10:
+                System.out.println("Октябрь");
+            case 11:
+                System.out.println("Ноябрь");
+            case 12:
+                System.out.println("Декабрь");
+
+
+        }
         }
     }
 
