f75ca75d8dbf3fc81e199bdcf5e00ad903ad4ae3
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-06 19:45:39 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -16,19 +16,13 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        if (a > b) {
-            if (a > c) {
-                System.out.println(a);
-            }else {
-                System.out.println(c);
-            }
-        } else {
-            if (b > c) {
-                System.out.println(b);
-            }
-            else {
-                System.out.println();
-            }
+        if (a > b)
+            if (a > c) System.out.println(a);
+            else System.out.println(c);
+
+        else if (b > c) System.out.println(b);
+           else System.out.println();
+
         }
     }
-}
+
