8197e65b151b413d08e8cc7059b960f4f862498c
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-06 19:29:46 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -1,7 +1,17 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        int nextInt = scanner.nextInt();
+        if (nextInt > 5){
+            System.out.println("nextInt > 5");
+            System.out.println("!");
+        } else {
+            System.out.println("nextInt < 5");
+            System.out.println("!");
+        }
     }
 }
