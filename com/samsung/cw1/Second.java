8c44c6d511aefb73ff93762785727fc001a2028e
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-07 21:12:37 +0300

    END

--- com/samsung/cw1/Second.java
+++ com/samsung/cw1/Second.java
@@ -1,4 +1,46 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Second {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b,c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        if(a < b && a < c) {
+            System.out.print(a);
+            if(b > c){
+                System.out.print( " " + c);
+                System.out.print(" " +b);
+
+            } else{
+                System.out.print(" " +b);
+                System.out.print(" " +c);
+            }
+        }
+        else if(b < a && b < c){
+            System.out.print(b);
+            if(a > c){
+                System.out.print(" " +c);
+                System.out.print(" " +a);
+            } else{
+                System.out.print(" " +a);
+                System.out.print(" " +c);
+            }
+
+
+        }else if(c < a && c < b){
+            System.out.print(c);
+            if(a > b){
+                System.out.print(" " +b);
+                System.out.print(" " +a);
+            } else{
+                System.out.print(" " +a);
+                System.out.print(" " +b);
+            }
+        }
+
+    }
 }
