b19f92fb86dcc787ad352436e53a145e3283ea0e
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-11 18:24:14 +0300

    END

--- com/samsung/cw1/Third.java
+++ com/samsung/cw1/Third.java
@@ -3,8 +3,22 @@ package com.samsung.cw1;
 import java.util.Scanner;
 
 public class Third {
-    public static void main(String[] args){
-        Scanner sc = New Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        int a = scanner.nextInt();
+
+        System.out.print(a + " ");
+
+        if((a / 10) % 10 == 1 || a % 10 >= 5 || a % 10 == 0) {
+            System.out.println("TORTOV");
+        } else if(a % 10 == 1) {
+            System.out.println("TORT");
+        } else {
+            System.out.println("TORTA");
+        }
 
     }
+
 }
