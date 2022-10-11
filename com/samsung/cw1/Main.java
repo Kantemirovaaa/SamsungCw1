2f491a4f893f2b5a6f9d6d9acf47c88aab65a40c
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-11 20:29:43 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -1,7 +1,49 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        /*System.out.println();//новая строчка
+        double aDouble = 3.343443343434;
+        System.out.print("jfojfoe" + "\n");//это как println
+        System.out.printf("%.3f", aDouble);//сокращает до нужного кол-ва знаков
+        System.out.printf("%.3f | %.5f", aDouble, aDouble);//String.format(), metanit
+        StringBuilder stringBuilder = new StringBuilder();//скок угодно раз будет добавлять строе
+        */
+
+
+        Scanner scanner = new Scanner(System.in);
+        //int n = scanner.nextInt();
+        //int count = 1;
+        /*do {
+            n = scanner.nextInt();
+            count++;
+        }while (n>=0);
+            n = scanner.nextInt();
+            count++;*/
+        /*while (n < 100) {
+            System.out.println(n);
+            n ++;
+        }*/
+        /*do{
+            System.out.println(n);
+        }while(n < 100);*/
+        /*int n, count = 0;
+        do{
+            n = scanner.nextInt();
+            count++;
+        } while(n >=0);
+        System.out.println(count);
+        scanner.close();*/
+        int n = scanner.nextInt();
+        while(n < 100){
+            n ++;
+            if(n == 71) continue;
+            System.out.println(n);
+        }
+
+
+
     }
 }
