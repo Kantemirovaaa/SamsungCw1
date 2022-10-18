e1f927116ba3c6688dd8e3e8f574533d5def55bc
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-18 19:22:55 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -1,7 +1,30 @@
 package com.samsung.cw1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        int number = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == x){
+                number = i + 1;
+                System.out.println(number);
+                break;
+            }
+        }
+        if(number != -1){
+            System.out.println(number);
+        }else{
+            System.out.println("NO");
+        }
+
+
     }
 }
