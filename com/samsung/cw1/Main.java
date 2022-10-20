2f0b716865b20926181c4c0b10f2cd3e6cc60710
@kdeliza <94979208+Kantemirovaaa@users.noreply.github.com>
2022-10-20 19:41:15 +0300

    END

--- com/samsung/cw1/Main.java
+++ com/samsung/cw1/Main.java
@@ -2,6 +2,18 @@ package com.samsung.cw1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int[][] arr = new int[5][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int)(Math.random() * 100);
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+
+        }
     }
 }
